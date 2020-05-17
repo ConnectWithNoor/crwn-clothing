e645bf833ec4fc645ff4b4f69984c5d537952071
Noor Muhammad <connectwithnoor1@gmail.com>
2020-05-17 17:28:54 +0500

    created: header componenet and added routing

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 import './App.css';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/'>
-        <Homepage />
-      </Route>
-      <Route path='/shop'>
-        <ShopPage />
-      </Route>
-    </Switch>
+    <>
+      <Header />
+
+      <Switch>
+        <Route exact path='/'>
+          <Homepage />
+        </Route>
+        <Route path='/shop'>
+          <ShopPage />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
