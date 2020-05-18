a67c0b7a630c62adc4a08d5ad779f246caeda2af
Noor Muhammad <connectwithnoor1@gmail.com>
2020-05-18 17:25:18 +0500

    setup: login and logout trigger and view

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/signin-and-signup/signin-and-signup.compoent';
+import { auth } from './firebase/fireabase.utils';
 
 import './App.css';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => setCurrentUser(user));
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
-      <Header />
+      <Header currentUser={currentUser} />
 
       <Switch>
         <Route exact path='/'>
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { auth } from '../../firebase/fireabase.utils';
 
 import './header.styles.scss';
 
-const Header = () => {
+const Header = ({ currentUser }) => {
   return (
     <div className='header'>
       <Link className='logo-container' to='/'>
@@ -18,9 +19,15 @@ const Header = () => {
         <Link className='option' to='/contact'>
           contact
         </Link>
-        <Link className='option' to='/signin'>
-          signin
-        </Link>
+        {currentUser ? (
+          <div className='option' onClick={() => auth.signOut()}>
+            Sign Out
+          </div>
+        ) : (
+          <Link className='option' to='/signin'>
+            signin
+          </Link>
+        )}
       </div>
     </div>
   );
