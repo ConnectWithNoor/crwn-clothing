c64d29d12da40c58ce1b15dc9054f119c83457de
Noor Muhammad <connectwithnoor1@gmail.com>
2020-05-21 15:49:39 +0500

    cart-item component to display cart items on cart-dropdown

--- src/components/cart/cart-dropdown.component.jsx
+++ src/components/cart/cart-dropdown.component.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
+
 const CartDropdown = () => {
+  const { cartItems } = useSelector((state) => state.cart);
   return (
     <div className='cart-dropdown'>
-      <div className='cart-items' />
+      <div className='cart-items'>
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CustomButton>Go to checkout</CustomButton>
     </div>
   );
